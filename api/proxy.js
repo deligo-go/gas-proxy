85fb0180a0e1bd5a0938eb3974e4b196b56c93aa
DG <deligovv@gmail.com>
2025-12-10 01:21:03 +0530

    Add MIME type handling to proxy request

--- api/proxy.js
+++ api/proxy.js
@@ -6,6 +6,27 @@ const { URL } = require('url');
 const GAS_BASE = 'https://script.google.com/macros/s/AKfycbxjR1NDJlHbktoEAmA1t-m1Lphe_gV7yqI4UR99ju5WRnkFkIIrhqopz2VEiVNRQ9Pn7g/exec';
 const IGNORED_PARAMS = ['pli', 'authuser', 'ifk'];
 
+// Function to determine MIME type from file extension
+function getMimeType(url) {
+    const ext = url.split('.').pop().split('?')[0].toLowerCase();
+    const mimeTypes = {
+        'css': 'text/css',
+        'js': 'application/javascript',
+        'json': 'application/json',
+        'png': 'image/png',
+        'jpg': 'image/jpeg',
+        'jpeg': 'image/jpeg',
+        'gif': 'image/gif',
+        'svg': 'image/svg+xml',
+        'ico': 'image/x-icon',
+        'woff': 'font/woff',
+        'woff2': 'font/woff2',
+        'ttf': 'font/ttf',
+        'eot': 'application/vnd.ms-fontobject'
+    };
+    return mimeTypes[ext] || 'text/html';
+}
+
 module.exports = (req, res) => {
     // Clean the request URL to remove Google-specific parameters
     const requestUrl = new URL(req.url, `https://${req.headers.host}`);
@@ -22,13 +43,16 @@ module.exports = (req, res) => {
     const targetUrl = finalGasUrl.toString();
     const parsedUrl = new URL(targetUrl);
 
+    // Determine correct MIME type from the request URL
+    const correctMimeType = getMimeType(req.url);
+
     const options = {
         hostname: parsedUrl.hostname,
         path: parsedUrl.pathname + parsedUrl.search,
         method: req.method,
         headers: {
             'User-Agent': req.headers['user-agent'] || 'Mozilla/5.0',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'Accept': '*/*',
         },
     };
 
@@ -36,15 +60,18 @@ module.exports = (req, res) => {
         // Set status code
         res.statusCode = proxyRes.statusCode || 200;
 
-        // Copy all headers except problematic security ones
+        // Copy headers but override Content-Type
         Object.keys(proxyRes.headers).forEach((key) => {
             const lowerKey = key.toLowerCase();
-            if (!['content-security-policy', 'x-frame-options', 'strict-transport-security'].includes(lowerKey)) {
+            if (!['content-security-policy', 'x-frame-options', 'strict-transport-security', 'content-type'].includes(lowerKey)) {
                 res.setHeader(key, proxyRes.headers[key]);
             }
         });
 
-        // Simply pipe the response
+        // Set correct Content-Type based on file extension
+        res.setHeader('Content-Type', correctMimeType);
+
+        // Pipe the response
         proxyRes.pipe(res);
     });
 
